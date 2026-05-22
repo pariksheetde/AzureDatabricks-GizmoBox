6fd32c828673a1fa3916773136356242192e863d
pariksheetde <depariksheet2@gmail.com>
2026-05-22 06:57:01 +0000

    COMMIT

--- 02.CircuitBox/02.LakeflowDeclarativePipeline/01.Customers/Silver/01.CustomersEnriched.sql
+++ 02.CircuitBox/02.LakeflowDeclarativePipeline/01.Customers/Silver/01.CustomersEnriched.sql
@@ -5,7 +5,8 @@ CREATE OR REFRESH STREAMING TABLE ECOMMERCE.SILVER.CUSTOMERS_ENRICHED
     CONSTRAINT valid_customer_first_name EXPECT (first_name IS NOT NULL) ON VIOLATION DROP ROW,
     CONSTRAINT valid_customer_last_name EXPECT (last_name IS NOT NULL) ON VIOLATION DROP ROW,
     CONSTRAINT valid_telephone EXPECT (length(telephone) >=10),
-    CONSTRAINT valid_email EXPECT (email IS NOT NULL),
+    -- CONSTRAINT valid_email EXPECT (email IS NOT NULL),
+    CONSTRAINT valid_email EXPECT (email IS NOT NULL) ON VIOLATION DROP ROW,
     CONSTRAINT valid_date_of_birth EXPECT (date_of_birth >= '1900-01-01')
 )
 TBLPROPERTIES ('layer' = 'silver')
@@ -16,8 +17,7 @@ split(customer_name,' ')[0] AS first_name,
 split(customer_name,' ')[1] AS last_name,
 date(date_of_birth),
 email,
-member_since,
 telephone,
-to_timestamp(created_timestamp, 'yyyy-MM-dd HH:mm:ss') AS created_timestamp,
+created_date,
 current_timestamp() AS load_timestamp
 FROM STREAM(LIVE.ECOMMERCE.BRONZE.CUSTOMERS_RAW)
